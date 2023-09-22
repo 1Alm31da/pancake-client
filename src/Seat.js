e8be852347b7648c965adbcee75130b6a665520e
1Alm31da <igalmeida@deloitte.pt>
2023-09-22 15:09:17 +0100

    transport

--- src/Seat.js
+++ src/Seat.js
@@ -7,7 +7,7 @@ export default function Seat() {
     const [rows, setRows] = useState({})
 
     useEffect(() => {
-        const s = io("https://earnest-pegasus-1ebe1c.netlify.app", { transports: ['websocket', 'polling', 'flashsocket'] });
+        const s = io("https://earnest-pegasus-1ebe1c.netlify.app", { transports: ['websocket'] });
         setSocket(s);
 
         s.on("inicial-seats", (inicialState) => {
