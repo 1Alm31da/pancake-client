724cea3b1c8a6fbf912243acf9ddb96038190d0f
1Alm31da <igalmeida@deloitte.pt>
2023-09-22 15:30:35 +0100

    oficial

--- src/Seat.js
+++ src/Seat.js
@@ -7,7 +7,13 @@ export default function Seat() {
     const [rows, setRows] = useState({})
 
     useEffect(() => {
-        const s = io("https://earnest-pegasus-1ebe1c.netlify.app");
+        //const s = io("https://earnest-pegasus-1ebe1c.netlify.app");
+        const s = io("https://earnest-pegasus-1ebe1c.netlify.app", {
+            withCredentials: true,
+            extraHeaders: {
+                "my-custom-header": "abcd"
+            }
+        });
         setSocket(s);
 
         s.on("inicial-seats", (inicialState) => {
